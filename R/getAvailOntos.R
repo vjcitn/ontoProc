a6f3b6acea42fb80d308ea19bbaa560dcade8f9d
reshs <reshs@channing.harvard.edu>
2023-07-27 15:36:19 -0400

    changing the column numbers to names

--- R/getAvailOntos.R
+++ R/getAvailOntos.R
@@ -9,7 +9,7 @@
 getAvailOntos = function() {
   ah = AnnotationHub::AnnotationHub()
   avail = AnnotationHub::query(ah, "ontoProc")
-  df =  as.data.frame(S4Vectors::mcols(avail))[,c(1,6)]
+  df =  as.data.frame(S4Vectors::mcols(avail))[,c("title", "description", "rdatadateadded")]
   df
 }
 
