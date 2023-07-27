9cf0cb2d1e4618d71ebbf4ce8d483b19d376567e
reshs <reshs@channing.harvard.edu>
2023-07-27 14:46:47 -0400

    fixed the errors in documentation

--- R/getAvailOntos.R
+++ R/getAvailOntos.R
@@ -1,6 +1,7 @@
 #'This function returns a list of available ontologies in the ontoProc package.
 #'@importFrom AnnotationHub AnnotationHub
 #'@importFrom AnnotationHub query
+#'@importFrom S4Vectors mcols
 #'@import ontologyIndex 
 #' @examples
 #' getAvailOntos()
@@ -8,7 +9,7 @@
 getAvailOntos = function() {
   ah = AnnotationHub::AnnotationHub()
   avail = AnnotationHub::query(ah, "ontoProc")
-  df =  as.data.frame(mcols(avail))[,c(1,6)]
+  df =  as.data.frame(S4Vectors::mcols(avail))[,c(1,6)]
   df
 }
 
