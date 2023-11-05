dbc3d0dfb22bb34e6752dbc984f06585275b796c
vjcitn <stvjc@channing.harvard.edu>
2023-11-05 15:10:08 -0500

    cache tool

---
+++ R/owl2cache.R
@@ -0,0 +1,22 @@
+#' cache an owl file accessible via URL
+#' @param cache BiocFileCache instance or equivalent
+#' @param url character(1)
+#' @note This function will check for presence of url in cache using bfcquery; 
+#' if a hit is found, returns the rpath associated with the last
+#' matching record.  etags can be available for use with bfcneedsupdate.
+#' @examples
+#' ca = BiocFileCache::BiocFileCache()
+#' hppa = owl2cache(ca, 
+#'    url="http://purl.obolibrary.org/obo/hp/releases/2023-10-09/hp-base.owl")
+#' setup_entities(hppa)
+#' @export
+owl2cache = function(cache = BiocFileCache::BiocFileCache(),
+   url = "http://purl.obolibrary.org/obo/cl.owl") {
+   inf = BiocFileCache::bfcquery(cache, url)
+   if (nrow(inf)>0) {
+     res = inf[nrow(inf),]
+     message(sprintf("resource %s already in cache from %s\n", res$rid, url))
+     return(res$rpath)
+     }
+   BiocFileCache::bfcadd(cache, rname = basename(url), fpath=url, rtype="web")
+}
--- inst/scripts/newpl.R
+++ inst/scripts/newpl.R
@@ -13,3 +13,26 @@ alluse = unique(unlist(anames))
 red = cl2[alluse]
 myo = ontology_index(parents=parents(red), name=labels(red))
 onto_plot2(myo, n3k)
+
+
+#' visualize ontology selection via onto_plot2, based on owlents
+#' @param x owlents instance
+#' @param y character() vector of entries in x$clnames
+#' @param \dots passed to onto_plot2
+#' @examples
+#' cl3k = c("CL:0000492", "CL:0001054", "CL:0000236", 
+#'   "CL:0000625", "CL:0000576", 
+#'   "CL:0000623", "CL:0000451", "CL:0000556")
+#' @export
+plot.owlents = function(x, y, ...) {
+  oky = intersect(y, x$clnames)
+  if (length(oky)<2) stop("not enough selected names present in x$clnames")
+  litc = x[y]
+  alitc = ancestors(litc)
+  anames = ancestors_names(alitc)
+  alluse = unique(unlist(anames))
+  red = x[alluse]
+  myo = ontologyIndex::ontology_index(parents=parents(red),
+         name=labels(red))
+  onto_plot2(myo, n3k, ...)
+}
