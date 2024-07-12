8b8a9fb2793b75b9a76cca5a84a1d8e54802b794
vjcitn <stvjc@channing.harvard.edu>
2024-07-12 15:09:42 -0400

    adding python config code

---
+++ R/zzz.R
@@ -0,0 +1,14 @@
+#' use doc at https://rstudio.github.io/reticulate/articles/python_dependencies.html
+
+#' user support for python module
+#' @param \dots as needed
+#' @param envname character(1) arbitrary
+#' @export
+install_owlready2 <- function(..., envname = "r-owlready2") {
+  reticulate::py_install("owlready2", envname = envname, ...)
+}
+
+
+.onLoad <- function(...) {
+  use_virtualenv("r-owlready2", required = FALSE)
+}
